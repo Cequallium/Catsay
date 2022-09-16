edcea8a8f73a0a24ce2fb0e0ba40c05f55f960b4
Cequallium <adityaojha@outlook.in>
2022-09-16 18:55:10 +0530

    Optimize Build size + Add --help

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
-use std::env;
-use colored::*;
-
+use std::env::args;
+use colored::Colorize;
+use colored::ColoredString;
 
 fn main() {
-    // let args: Vec<_> = env::args().collect();
-    let args: Vec<_> = env::args().collect();
-    if args.len() == 3 {
-        catsay(&args[1], &args[2].parse::<i16>().unwrap());
-    } else {
+
+    let args: Vec<String> = args().collect();
+    
+    // spaghetti error handling / routing
+    if args[1] == "--help" {
+
+        // spacing
+        println!("");
+
+        println!("  {}","Usage: catsay <Phrase> <Color>".white());
+        print!("  {}","Colors:".black());
+        println!("  {}{}{}{}{}{}{}{}",
+                " 1 ".on_black(),
+                " 2 ".on_red(),
+                " 3 ".on_green(),
+                " 4 ".on_yellow(),
+                " 5 ".on_blue(),
+                " 6 ".on_magenta(),
+                " 7 ".on_cyan(),
+                " 8 ".on_white());
+    }
+    else if args.len() == 3 {
+
+        let arg2: &i8 = &args[2].parse::<i8>().unwrap();
+
+        if arg2 <= &8{
+            catsay(&args[1], arg2);
+        }
+        else {
+            catsay("<Color> has to be =<8", &2)
+        }
+    }
+    else {
         catsay("both <Phrase> & <Color> are required",&1);
     }
 }
 
-fn catsay(say: &str,color: &i16) {
+fn catsay(say: &str,color: &i8) {
 
     let line1: &str = "   /| ､      ";
     let line2: &str = "  (°､ ｡ 7    ";
@@ -20,6 +48,7 @@ fn catsay(say: &str,color: &i16) {
     let line4: &str = "   じしf_,)〳 ";
     let cat: [&str; 4] = [line1,line2,line3,line4];
 
+    println!(""); // blank space
     for i in 0..=3{
         if i == 1{
             println!("{} {}",colorize(cat[i],color), colorize(say, color));
@@ -30,7 +59,7 @@ fn catsay(say: &str,color: &i16) {
 
 }
 
-fn colorize(i: &str,color: &i16) -> ColoredString{
+fn colorize(i: &str,color: &i8) -> ColoredString{
     match color {
         1 => return i.black(),
         2 => return i.red(),
@@ -41,6 +70,6 @@ fn colorize(i: &str,color: &i16) -> ColoredString{
         7 => return i.cyan(),
         8 => return i.white(),
         // catch all
-        _ => std::process::exit(1)
+        _ => return "invalid color specified".red()
     }
 }
